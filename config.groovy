dd0aa86c05ada61f05509a5efbc5f1596b8f8a35
John <john@nobleapplications.com>
2023-10-27 17:28:22 -0400

    chore: comment out tests; refactor: update gradle file and groovy config

---
+++ config.groovy
@@ -0,0 +1,20 @@
+// Defaults
+indicator = 'local'
+
+environments {
+    local {
+        indicator = 'local'
+    }
+
+    test {
+        indicator = 'test'
+    }
+
+    dev {
+        indicator = 'dev'
+    }
+
+    prod {
+        indicator = 'prod'
+    }
+}
